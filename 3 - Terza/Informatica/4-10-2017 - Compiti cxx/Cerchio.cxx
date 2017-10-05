ac7d0a43bdf90471ba93c4feddb96a3191cb6483
Kevin Delugan - Portatile <bisumto@gmail.com>
2017-10-05 07:39:32 +0200

    Correzione compiti informatica 04-10-2017

--- 3 - Terza/Informatica/4-10-2017 - Compiti cxx/Cerchio.cxx
+++ 3 - Terza/Informatica/4-10-2017 - Compiti cxx/Cerchio.cxx
@@ -3,10 +3,10 @@ using namespace std;
 
 int main()
 {
-	int r,A,C,pi;
+	int r,A,C;
+	const float pi=3.14;
 	cout<<"Inserisci la dimensione del tuo raggio.";
 	cin>>r;
-	pi=3.14;
 	C=2*r*pi;
 	A=r*r*pi;
 	cout<<"La circonferenza misurera "<<C<<", mentre l'area misura "<<A<<".";
