815a2c08e170315d4a0eeba894bc75dd5e0d349f
Kevin Delugan - Portatile <bisumto@gmail.com>
2017-10-19 21:50:53 +0200

    Correzione compiti informatica 19-10-2017

--- 3 - Terza/Informatica/19-10-2017 - Esercizi/19-10-2017 - Incasso giornaliero.cxx
+++ 3 - Terza/Informatica/19-10-2017 - Esercizi/19-10-2017 - Incasso giornaliero.cxx
@@ -7,16 +7,18 @@ float somma(float p,float c)
 	return p; 
 }
 
-void media(float p,int n)
+float media(float p,int n)
 {
 	float m;
-	m=p/(float)n--;
+	n--;
+	m=p/(float)n;
 	return m;
 }
 
 int main()
 {
 	float incasso, parziale;
+	int i;
 	i=1;
 	parziale=0;
 	do {
@@ -25,10 +27,12 @@ int main()
 			cout<<"\033[37mQuando hai terminato e vuoi sapere la media, immetti -1\033[0m"<<endl;
 		}
 		cin>>incasso;
+		if (incasso==-1){
+			cout<<"La media degli incassi è di €"<<media(parziale,i)<<endl;
+			return 0;
+		}
 		parziale=somma(parziale,incasso);
 		cout<<"Attualmente il totale degli incassi ammonta a €"<<parziale<<endl;
 		i++;
-	}
-	cout<<"La media degli incassi è di €"<<media(parziale,i)<<endl;
-	return 0;
+	} while(incasso!=-1);
 }
