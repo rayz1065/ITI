3593ba834152bf6a8892246f6422381a6c3fc467
rayz1065 <37779815+rayz1065@users.noreply.github.com>
2018-03-25 20:40:34 +0200

    25-03-2018 Queens soluzione 36ms
    Questa soluzione da come tempo di esecuzione maggiore 36 millisecondi grazie ad alcuni accorgimenti
    
    // btw non so come funziona github quindi probabilmente ho fatto qualcosa di molto sbagliato

--- 3 - Terza/#Extra/Candriai/25-03-2018 - Queen [DF]/25-03-2018 - Queen - SOLUZIONE.cpp
+++ 3 - Terza/#Extra/Candriai/25-03-2018 - Queen [DF]/25-03-2018 - Queen - SOLUZIONE.cpp
@@ -1,79 +1,80 @@
-#include <fstream>
-#include <vector>
+// Tempo peggiore: 36 millisecondi
+#include <bits/stdc++.h> //libreria per tutte le librerie
+#define MAXN 16
 using namespace std;
 
-struct coord{
-    int x;
-    int y;
-};
+int N, M, Q;
+int scacchiera[MAXN][MAXN];
+int movr[] = {-1, -1, 0, 1, 1, 1, 0, -1}, movc[] = {0, 1, 1, 1, 0, -1, -1, -1}; // 2 array per gestire i movimenti
 
-vector<vector<bool> >mappa;
-vector<coord> possibili;
-int M,N,Q,minore;
+int am = 0; // ammonto della migliore soluzione
+int bestS = 0; // soluzione migliore
 
-
-
-/*void filtra(int x,int y){
-     mappa[x][y]=0
-     for (int i=0;i<M;i++) mappa[i][y]=0;
-     for (int i=0;i<N;i++) mappa[x][i]=0;
-     if (M<N) for (int i=x-y;i<M;i++) mappa[i][i]=0;
-     else for (int i=x-y;i<N;i++) mappa[i][i]=0;
-}*/
-void escludi(int x, int y) {
-	for (int j = 0; j < n; j++) {
-		mappa[x][j] = false;
-	}
-	for (int j = 0; j < m; j++) {
-		mappa[j][y] = false;
-	}
-	for (int j = 0; j < n; j++) {
-		int x2 = y-x+j;
-		int y2 = x+y-j;
-		if (x2 < n && x2 >= 0) {
-			mappa[x2][j] = false;
-		}
-		if (y2 < n && y2 >= 0) {
-			mappa[j][y2] = false;
-		}
-	}
+bool inRange(int r, int c){ // controlla se la cella è dentro i limiti della scacchiera
+    return (r>=0 && c>=0 && r < N && c < M);
 }
 
-void test(){
-    do {
-    if (m<=n){
-        for (int i=m-1;i=0;i--) if (tenta(i)) possibili++;
-    }else{
-        for (int i=n-1;i>0;i--) if (tenta(i)) possibili++;
-    }} while (!trovato);
+void escludi(int r, int c, int a){ // posiziona o toglie la queen in posizione r c
+    scacchiera[r][c]+= a;
+    for(int m = 0; m < 8; m++){
+        int newr = r, newc = c;
+        while(inRange(newr + movr[m], newc + movc[m])){
+            newr+= movr[m], newc+= movc[m];
+            scacchiera[newr][newc]+=a;
+        }
+    }
 }
 
-main(){
-    ifstream in;
-	in.open("input.txt");
-	assert(in);
-	in>>M>>N>>Q;
-	mappa=vector<vector<bool> >(M,vector<bool>(N,1));
-	if (M<N) minore=M;
-	else minore=N;
-	possibili=vector<coord>();
-	for (int i=0;i<Q;i++){
-        int x,y;
-        in>>x>>y;
-        escludi(x,y);
-	}
+void risolvi(int r, int c, int q){ // funzione risolutiva
+    while(r >= N || c >= M || scacchiera[r][c] != 0){ // trova la prima casella libera
+        c++;
+        if(c >= M){
+            c = 0;
+            r++;
+            if(r >= N){ // se supera la tabella vuol dire che ha trovato una possibile soluzione
+                if(q >= bestS){
+                    if(q > bestS){ // potrebbe essere una soluzione migliore, in caso la impostiamo
+                        am = 0;
+                        bestS = q;
+                    }
+                    am++;
+                }
+                return;
+            }
+        }
+    }
+
+    int rim = 0; // calcoliamo quante potenziali regine possiamo scegliere sommariamente
+    for(int i = r; i < N; i++)
+        for(int j = 0; j < M; j++)
+            if(scacchiera[i][j] == 0 && (i > r || j > c)){
+                j = M; // per evitare di arrivare in fondo alla riga
+                rim++;
+            }
+
+    if(q + rim + 1 < bestS) return; // se le regine potenziali sono troppo poche torniamo indietro
+
+    escludi(r, c, 1); // mettiamo la regina, computiamo la soluzione e poi la rimuoviamo
+    risolvi(r + 1, 0, q + 1);
+    escludi(r, c, -1);
+
+    if(q + rim < bestS) return; // se le regine potenziali sono troppo poche togliendo quella presente nella casella torniamo indietro
+
+    risolvi(r, c + 1, q); // computiamo la soluzione se non mettiamo la regina
+}
+
+int main(){
+    fstream in, out;
+    in.open("input.txt", ios::in); // input
+    in>>N>>M>>Q; // non ha importanza in che ordine prendiamo righe e colonne finchè le usiamo con lo stesso significato
+    for(int q = 0; q < Q; q++){
+        int r, c;
+        in>>r>>c;
+        escludi(r - 1, c - 1, 1);
+    }
     in.close();
-
-    /////////////////////
-    for(int i=0;i<M;i++)
-        for (int j=0;j<N;j++)
-
-
-	tenta(0);
-	ofstream out;
-    out.open("output.txt");
-    assert(out);
-	out << n_queen;
-	out.close();
-	return 0;
+    risolvi(0, 0, 0); // risolviamo a partire dalla prima casella con 0 regine
+    out.open("output.txt", ios::out);
+    out<<bestS<<" "<<am; // stampiamo la soluzione migliore e quante ne abbiamo trovate
+    out.close();
 }
